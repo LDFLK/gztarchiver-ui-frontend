a48c378b2aca70d75811423920c6e7eb39f7b41d
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-08-12 17:32:12 +0530

    update : content updates

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
    return ( 
     <div className="p-6">
   <div className="bg-white p-6 rounded-xl shadow-md">
-    <h1 className="text-2xl font-bold mb-4">Home</h1>
+    <h1 className="text-2xl font-bold mb-4">🧪 gztarchiver</h1>
 
     <h2 className="text-lg font-semibold mb-3">Available Years:</h2>
 
@@ -73,7 +73,7 @@ const Home = () => {
           onClick={() => handleCollectionClick(collection)}
           className="px-4 py-2 bg-gray-50 border border-gray-200 rounded-lg cursor-pointer hover:bg-blue-50 hover:border-blue-300 transition-colors duration-200"
         >
-          {extractYear(collection)}
+         📍 {extractYear(collection)}
         </li>
       ))}
     </ul>
