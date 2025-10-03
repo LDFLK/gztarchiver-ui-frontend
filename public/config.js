c158838186748eb88801a97ef5229e8b1bd18213
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-10-03 17:04:18 +0530

    feat : make every state shareable with unique urls (#7)
    * feat : removing proxy settings for dev
    
    * feat : adding the config.js
    
    * fix : changing config paths
    
    * fix : fix bugs on search filters
    
    * fix : fix bugs
    
    * feat : implement unique url for searching , making each state sharable

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
-// window.configs = {
-//   apiUrl: '/choreo-apis/orgchart2/go-rest/v1',
-// };
+window.configs = {
+  apiUrl: 'https://web-production-a5197.up.railway.app'
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import Home from './pages/home';
-import CollectionPage from './pages/collection';
-import SingleDoc from './pages/single_doc';
-
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/:collection" element={<CollectionPage/>} />
-        <Route path="/:collection/:doc_id" element={<SingleDoc/>}/>
       </Routes>
     </Router>
   )
